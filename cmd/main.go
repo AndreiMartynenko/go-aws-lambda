5365b6d2bad956780a34c5d2d4a8fd6283b4c329
AndrewLondon <andrewmart@mail.com>
2023-10-10 00:07:42 +0100

    Updated

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,21 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func main() {
 	//creating a session
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
-		Region: aws.String(region)
-	})
+		Region: aws.String(region)})
+	//handling error
+	if err != nil {
+		return
+	}
+	//DynamoDB
+	dynaClient = dynamodb.New(awsSession)
+	//Lambda package
+	lambda.Start(handler)
 
 }
