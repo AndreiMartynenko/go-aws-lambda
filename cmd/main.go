d493ba36f0c59e7a2079698a92fd628fd6df913f
AndrewLondon <andrewmart@mail.com>
2023-10-09 23:53:43 +0100

    Updated

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,15 @@ package main
 import (
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func main() {
 	//creating a session
 	region := os.Getenv("AWS_REGION")
-	awsSession, err := session.NewSession
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)
+	})
 
 }
